f78176c2c4d5558c33e6548aeab144b51c6f9e1f
98LucjaRomanska <146941951+98LucjaRomanska@users.noreply.github.com>
2023-10-10 22:45:42 +0200

    Create hello.py

---
+++ 02-TypesAndVariables/hello.py
@@ -0,0 +1,48 @@
+x = 5
+print(x)
+print(type(x))
+
+
+#Excercise 2
+name = input('Enter your name\n')
+print("Hello " + name)
+
+#Excercise 3 
+x = input("Enter hours:\n")
+hour= input(x)
+y= input("Enter rate:\n")
+rate = input(y)
+int(hour)
+int(rate)
+print(int(hour))
+
+rate = input("Enter Rate:\n")
+print(type(rate))
+pay = hour * rate 
+print("That's your pay: " + pay )
+
+#Exercise 4
+width = 17
+height = 12.0
+print(type(width))
+print(type(height))
+
+print(height)
+print(type(height))
+
+width = float(width)
+print(width)
+print(type(width))
+
+mod = width//2 
+print(mod)
+print(type(mod))
+
+dv = width/2.0
+print(dv)
+print(type(dv))
+
+dvh =height/3
+print(dvh)
+print(type(dvh))
+
